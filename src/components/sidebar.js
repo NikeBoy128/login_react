26106faf22ee8b4c31f6d87297b9878b0825116d
Harold Mosquera <haroldmosquera739@gmail.com>
2023-11-25 15:17:18 -0500

    Fix sidebar rendering issue

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import {
   CDBSidebar,
   CDBSidebarContent,
@@ -8,34 +8,46 @@ import {
   CDBSidebarMenuItem,
 } from 'cdbreact';
 import { NavLink } from 'react-router-dom';
-
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 const Sidebar = () => {
+  const [user, setUser] = useState(null);
+const [error, setError] = useState('');
+useEffect(() => {
+  const fetchUserData = async () => {
+    try {
+      const response = await axios.get('https://backen-diplomado-51d51f42ca0d.herokuapp.com/usuario_autenticado/', {
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
+      });
+      setUser(response.data);
+    } catch (err) {
+      setError('Failed to fetch user data');
+    }
+  }
+
+  fetchUserData();
+}, []);
   return (
     <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
       <CDBSidebar textColor="#fff" backgroundColor="#333">
         <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
           <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
-            Sidebar
+            Bienvenido : {user ? user.username : 'Loading...'}
           </a>
         </CDBSidebarHeader>
 
         <CDBSidebarContent className="sidebar-content">
           <CDBSidebarMenu>
             <NavLink exact to="/" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="columns">Dashboard</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="user">Usuarios</CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/tables" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="table">Tables</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="car">Autos</CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/profile" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="user">Profile page</CDBSidebarMenuItem>
-            </NavLink>
-            <NavLink exact to="/analytics" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="chart-line">Analytics</CDBSidebarMenuItem>
-            </NavLink>
-
-            <NavLink exact to="/hero404" target="_blank" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="exclamation-circle">404 page</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="plane">Viajes</CDBSidebarMenuItem>
             </NavLink>
           </CDBSidebarMenu>
         </CDBSidebarContent>
@@ -46,7 +58,7 @@ const Sidebar = () => {
               padding: '20px 5px',
             }}
           >
-            Sidebar Footer
+            JJplus services
           </div>
         </CDBSidebarFooter>
       </CDBSidebar>
