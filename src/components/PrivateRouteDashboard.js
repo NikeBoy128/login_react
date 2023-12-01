3cf1956d102708a239f2c6930341f0a9f6fa8028
ShiroKurorex <danieale109@gmail.com>
2023-12-01 00:54:47 -0500

    muchos cambios melos

--- src/components/PrivateRouteDashboard.js
+++ src/components/PrivateRouteDashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router';
 import { useAuth } from '../contexts/AuthContext';
 import Dashboard from './Dashboard';
+
 export default function PrivateRouteDashboard() {
   const { user } = useAuth()
   return user ? <Dashboard/> : <Navigate to='/signin'/>
