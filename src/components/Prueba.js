bd47d43d5d1e13219890c543061433c91545631c
Harold Mosquera <haroldmosquera739@gmail.com>
2023-12-02 11:55:58 -0500

    Add react-data-table-component package

--- src/components/Prueba.js
+++ src/components/Prueba.js
@@ -1,51 +1,112 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Card, Container, Row, Col, Button, Form } from 'react-bootstrap';
-import Sidebar from './sidebar';
-import { Link } from 'react-router-dom'; // Asegúrate de importar Link desde react-router-dom
-import 'bootstrap/dist/css/bootstrap.css';
-
+import React, { useState, useEffect,useRef } from "react";
+import axios from "axios";
+import { Card, Container, Row, Col, Button, Form } from "react-bootstrap";
+import Sidebar from "./sidebar";
+import { Link } from "react-router-dom"; 
+import "bootstrap/dist/css/bootstrap.css";
+import DataTable from "react-data-table-component";
+import { Modal } from 'react-bootstrap';
 export default function Prueba() {
   const [data, setData] = useState([]);
   const [editedData, setEditedData] = useState({});
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
+  const [filterText, setFilterText] = useState('');
+  const [filteredItems, setFilteredItems] = useState([]);
+  const filterTimeout = useRef(null);
+  const [showModal, setShowModal] = useState(false);
+  const [showCreateModal, setShowCreateModal] = useState(false);
+  const [newData, setNewData] = useState({});
 
+  const columns = [
+    { name: 'Id', selector: 'id' },
+    { name: 'Marca', selector: 'marca' },
+    { name: 'Cantidad Pasajeros', selector: 'cantidad_pasajeros' },
+    { name: 'Placa', selector: 'placa' },
+    {
+      name: 'Acciones',
+      cell: row => (
+        <div>
+          <Button variant="warning" onClick={() => handleEdit(row)}>Editar</Button>
+          <Button variant="danger" onClick={() => handleDelete(row.id)}>Eliminar</Button>
+        </div>
+      ),
+    },
+  ];
+  const SubHeaderComponent = () => {
+    return (
+      <div>
+        Buscar: <input type='text' value={filterText} onChange={e => setFilterText(e.target.value)} />
+      </div>
+    );
+  };
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://backen-diplomado-51d51f42ca0d.herokuapp.com/autos/');
+        const response = await axios.get(
+          "https://backen-diplomado-51d51f42ca0d.herokuapp.com/autos/"
+        );
         setData(response.data);
       } catch (err) {
-        setError('Failed to fetch data');
+        setError("Failed to fetch data");
       }
     };
 
     fetchData();
   }, []);
+  useEffect(() => {
+    const filtered = data.filter(item => item.marca.toLowerCase().includes(filterText.toLowerCase()));
+    setFilteredItems(filtered);
+  }, [data, filterText]);
 
   const handleDelete = async (id) => {
     try {
-      await axios.delete(`https://backen-diplomado-51d51f42ca0d.herokuapp.com/autos/${id}`);
-      setData(data.filter(item => item.id !== id));
+      await axios.delete(
+        `https://backen-diplomado-51d51f42ca0d.herokuapp.com/autos/${id}`
+      );
+      setData(data.filter((item) => item.id !== id));
     } catch (err) {
-      setError('Failed to delete item');
+      setError("Failed to delete item");
     }
   };
 
   const handleEdit = (item) => {
     setEditedData(item);
+    setShowModal(true);
   };
 
   const handleUpdate = async () => {
     try {
-      await axios.put(`https://backen-diplomado-51d51f42ca0d.herokuapp.com/autos/${editedData.id}`, editedData);
-      const updatedData = data.map(item => (item.id === editedData.id ? editedData : item));
+      await axios.put(
+        `https://backen-diplomado-51d51f42ca0d.herokuapp.com/autos/${editedData.id}`,
+        editedData
+      );
+      const updatedData = data.map((item) =>
+        item.id === editedData.id ? editedData : item
+      );
       setData(updatedData);
       setEditedData({});
     } catch (err) {
-      setError('Failed to update item');
+      setError("Failed to update item");
     }
   };
+  const handleCreate = () => {
+    setNewData({});
+    setShowCreateModal(true);
+  };
+  const handleCreateSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        `https://backen-diplomado-51d51f42ca0d.herokuapp.com/autos/`,
+        newData
+      );
+      setData([...data, response.data]);
+      setShowCreateModal(false);
+    } catch (err) {
+      setError("Failed to create item");
+    }
+  };
+  
 
   return (
     <div style={{ height: '100vh' }}>
@@ -57,64 +118,106 @@ export default function Prueba() {
           <Col sm={9}>
             <div className="dashboard-content">
               <div className="container mt-4 shadow-lg p-3 mb-5 bg-body rounded">
-                <table className="table table-bordered table-striped">
-                  <thead>
-                    <tr>
-                      <th>Id</th>
-                      <th>Marca</th>
-                      <th>Cantidad Pasajeros</th>
-                      <th>Placa</th>
-                      <th>Acciones</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {data.map(item => (
-                      <tr key={item.id}>
-                        <td>{item.id}</td>
-                        <td>
-                          {editedData.id === item.id ? (
-                            <Form.Control
-                              type="text"
-                              value={editedData.marca}
-                              onChange={(e) => setEditedData({ ...editedData, marca: e.target.value })}
-                            />
-                          ) : (
-                            item.marca
-                          )}
-                        </td>
-                        <td>
-                          {editedData.id === item.id ? (
-                            <Form.Control
-                              type="text"
-                              value={editedData.cantidad_pasajeros}
-                              onChange={(e) => setEditedData({ ...editedData, cantidad_pasajeros: e.target.value })}
-                            />
-                          ) : (
-                            item.cantidad_pasajeros
-                          )}
-                        </td>
-                        <td>{item.placa}</td>
-                        <td>
-                          {editedData.id === item.id ? (
-                            <Button variant="success" onClick={handleUpdate}>Guardar</Button>
-                          ) : (
-                            <Button variant="warning" onClick={() => handleEdit(item)}>Editar</Button>
-                          )}
-                          <Button variant="danger" onClick={() => handleDelete(item.id)}>Eliminar</Button>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-                {error && <Card.Text>{error}</Card.Text>}
-                <Link to="/CrearAuto">
-                  <Button>Crear Auto</Button>
-                </Link>
+                <DataTable
+                  title="Autos"
+                  columns={columns}
+                  data={filteredItems}
+                  pagination 
+                  subHeader
+                  subHeaderComponent={<SubHeaderComponent />}
+                />
+                {error && <p>{error}</p>}
+                <Button onClick={() => setShowCreateModal(true)}>Crear Auto</Button>
+                
               </div>
             </div>
           </Col>
         </Row>
       </Container>
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Editar Auto</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleUpdate}>
+            <Form.Group controlId="marca">
+              <Form.Label>Marca</Form.Label>
+              <Form.Control
+                type="text"
+                value={editedData.marca}
+                onChange={(e) =>
+                  setEditedData({ ...editedData, marca: e.target.value })
+                }
+              />
+            </Form.Group>
+            <Form.Group controlId="cantidad_pasajeros">
+              <Form.Label>Cantidad Pasajeros</Form.Label>
+              <Form.Control
+                type="text"
+                value={editedData.cantidad_pasajeros}
+                onChange={(e) =>
+                  setEditedData({ ...editedData, cantidad_pasajeros: e.target.value })
+                }
+              />
+            </Form.Group>
+            <Form.Group controlId="placa">
+              <Form.Label>Placa</Form.Label>
+              <Form.Control
+                type="text"
+                value={editedData.placa}
+                onChange={(e) =>
+                  setEditedData({ ...editedData, placa: e.target.value })
+                }
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Update
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+      <Modal show={showCreateModal} onHide={() => setShowCreateModal(false)}>
+  <Modal.Header closeButton>
+    <Modal.Title>Crear Auto</Modal.Title>
+  </Modal.Header>
+  <Modal.Body>
+    <Form onSubmit={handleCreateSubmit}>
+      <Form.Group controlId="marca">
+        <Form.Label>Marca</Form.Label>
+        <Form.Control
+          type="text"
+          value={newData.marca || ''}
+          onChange={(e) =>
+            setNewData({ ...newData, marca: e.target.value })
+          }
+        />
+      </Form.Group>
+      <Form.Group controlId="cantidad_pasajeros">
+        <Form.Label>Cantidad Pasajeros</Form.Label>
+        <Form.Control
+          type="text"
+          value={newData.cantidad_pasajeros || ''}
+          onChange={(e) =>
+            setNewData({ ...newData, cantidad_pasajeros: e.target.value })
+          }
+        />
+      </Form.Group>
+      <Form.Group controlId="placa">
+        <Form.Label>Placa</Form.Label>
+        <Form.Control
+          type="text"
+          value={newData.placa || ''}
+          onChange={(e) =>
+            setNewData({ ...newData, placa: e.target.value })
+          }
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Crear
+      </Button>
+    </Form>
+  </Modal.Body>
+</Modal>
     </div>
   );
 }
